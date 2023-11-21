378486a1ebf68c1478c36df6a78288e9f0bdce24
Riste Micev <riste.micev1@gmail.com>
2023-11-21 15:16:21 +0100

    Creating Events Module

---
+++ database/migrations/2023_11_21_134026_create_events_tables.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTables extends Migration
+{
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            // this will create an id, a "published" column, and soft delete and timestamps columns
+            createDefaultTableFields($table);
+
+            $table->integer('position')->unsigned()->nullable();
+            $table->dateTimeTz('event_date')->nullable();
+
+            // add those 2 columns to enable publication timeframe fields (you can use publish_start_date only if you don't need to provide the ability to specify an end date)
+            // $table->timestamp('publish_start_date')->nullable();
+            // $table->timestamp('publish_end_date')->nullable();
+        });
+
+        Schema::create('event_translations', function (Blueprint $table) {
+            createDefaultTranslationsTableFields($table, 'event');
+            $table->string('title', 200)->nullable();
+            $table->text('description')->nullable();
+            $table->string('event_type', 200)->nullable();
+            $table->string('event_location', 200)->nullable();
+            $table->string('event_organizer', 200)->nullable();
+        });
+
+        Schema::create('event_slugs', function (Blueprint $table) {
+            createDefaultSlugsTableFields($table, 'event');
+        });
+
+
+    }
+
+    public function down()
+    {
+
+        Schema::dropIfExists('event_translations');
+        Schema::dropIfExists('event_slugs');
+        Schema::dropIfExists('events');
+    }
+}
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <title>Obalni Alpinistični Klub</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <link href="{{asset('assets/css/styles.css')}}" rel="stylesheet">
+
+</head>
+
+<body>
+
+@include('components.navbar')
+
+@yield('content')
+
+@include('components.scripts')
+
+</body>
+</html>
